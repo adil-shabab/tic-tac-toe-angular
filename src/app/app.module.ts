e2b4458fc499d505a7634f747cc217c27071ace0
Adil Shabab <adilshaba@gmail.com>
2022-07-27 08:50:05 -0700

    favicon changed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GameComponent } from './game/game.component';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,7 @@ import { GameComponent } from './game/game.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
